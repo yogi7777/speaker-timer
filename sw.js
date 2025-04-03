7627e558f0473d09fb2556760535ac2f86501950
yogi7777 <jw@wwj.ch>
2025-04-03 20:34:04 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,33 @@
+/*
+ * Speaker Timer Styles
+ * Autor: yogi7777
+ * Copyright © 2025 yogi7777 https://github.com/yogi7777. Alle Rechte vorbehalten.
+ */
+const CACHE_NAME = 'timer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/fullscreen.html',
+  '/styles.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          console.log('Cache geöffnet');
+          return cache.addAll(urlsToCache);
+        })
+        .catch(err => {
+          console.error('Fehler beim Cachen:', err);
+        })
+    );
+  });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
